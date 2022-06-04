eb5714e50bb2b760bd10239efa1119a871da67a2
Rodolpho Coller <rodolpho.coller12@gmail.com>
2022-06-04 19:03:46 -0300

    feat: login middleware

---
+++ src/schemas/loginSchema.ts
@@ -0,0 +1,9 @@
+import Joi from 'joi';
+
+const joiSchema = Joi.object({
+  email: Joi.string().empty().required().regex(/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/)
+    .messages({ 'string.pattern.base': '"email" must be a valid email' }),
+  password: Joi.string().empty().min(7).required(),
+});
+
+export default joiSchema;
