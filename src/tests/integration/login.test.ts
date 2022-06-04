960aff6b3604e845d14ed128280e9f2c24dca133
Rodolpho Coller <rodolpho.coller12@gmail.com>
2022-06-04 20:57:08 -0300

    test: login route

---
+++ src/tests/integration/login.test.ts
@@ -0,0 +1,57 @@
+/* eslint-disable no-undef */
+import * as sinon from 'sinon';
+import chai from 'chai';
+import bcrypt from 'bcryptjs';
+import chaiHttp from 'chai-http';
+import { after } from 'mocha';
+import app from '../../app';
+import { prisma } from '../../models/prisma';
+import * as data from '../testData/loginData';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('Integration test login', () => {
+  describe('Test login post route', () => {
+    after(() => { sinon.restore(); });
+
+    it('When everything goes well should return a token', async () => {
+      prisma.user.findUnique = sinon.stub().resolves(data.createdUserMock);
+      sinon.stub(bcrypt, 'compare').resolves(true);
+
+      const { status, body } = await chai
+        .request(app)
+        .post('/login')
+        .send(data.user);
+
+      expect(status).to.be.equal(200);
+      expect(body).to.have.key('token');
+      expect(body.token).to.be.a('string');
+    });
+
+    it('When database returns an unexpected error', async () => {
+      prisma.user.findUnique = sinon.stub().throws('Inside server error');
+
+      const { status, body: { message } } = await chai
+        .request(app)
+        .post('/login')
+        .send(data.user);
+
+      expect(status).to.be.equal(500);
+      expect(message).to.be.equal('Inside server error.');
+    });
+
+    it('When services returns an error', async () => {
+      prisma.user.findUnique = sinon.stub().resolves(undefined);
+
+      const { status, body: { message } } = await chai
+        .request(app)
+        .post('/login')
+        .send(data.user);
+
+      expect(status).to.be.equal(400);
+      expect(message).to.be.equal('Incorrect email or password.');
+    });
+  });
+});
---
+++ src/tests/unit/services/login.test.ts
@@ -0,0 +1,47 @@
+/* eslint-disable no-undef */
+import * as sinon from 'sinon';
+import chai from 'chai';
+import chaiHttp = require('chai-http');
+import bcrypt from 'bcryptjs';
+import * as service from '../../../services/loginService';
+import * as model from '../../../models/loginModel';
+import * as data from '../../testData/loginData';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('Test login services', () => {
+  describe('Test validate service', () => {
+    afterEach(() => { sinon.restore(); });
+
+    it('When the user already exists', async () => {
+      sinon.stub(model, 'readOne').resolves(data.createdUserMock);
+      try {
+        await service.validate(data.user);
+      } catch ({ message }) {
+        expect(message).to.be.equal('Incorrect email or password.');
+      }
+    });
+
+    it('When password does not match', async () => {
+      sinon.stub(model, 'readOne').resolves(data.createdUserMock);
+      sinon.stub(bcrypt, 'compare').resolves(false);
+
+      try {
+        await service.validate(data.user);
+      } catch ({ message }) {
+        expect(message).to.be.equal('Incorrect email or password.');
+      }
+    });
+
+    it('When everything goes well', async () => {
+      sinon.stub(model, 'readOne').resolves(data.createdUserMock);
+      sinon.stub(bcrypt, 'compare').resolves(true);
+
+      const token = await service.validate(data.user);
+
+      expect(token).to.be.a('string');
+    });
+  });
+});
