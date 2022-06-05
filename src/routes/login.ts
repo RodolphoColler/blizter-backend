86d74eb90b3e7835cbc35e2c249f92a448a8f1d4
Rodolpho Coller <rodolpho.coller12@gmail.com>
2022-06-04 21:59:24 -0300

    feat: validations on login route

--- src/routes/login.ts
+++ src/routes/login.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
 import * as controllers from '../controllers/loginController';
+import validateLogin from '../middlewares/validateLogin';
 
 const router = Router();
 
-router.post('/', controllers.validate);
+router.post('/', validateLogin, controllers.validate);
 
 export default router;
