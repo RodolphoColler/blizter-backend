c5a88a2d059ed0b33d6434720cc9dfe2c5e677c5
Rodolpho Coller <rodolpho.coller12@gmail.com>
2022-06-13 16:13:01 -0300

    test: expenditure read middleware

--- src/tests/unit/middewares/expenditure.test.ts
+++ src/tests/unit/middewares/expenditure.test.ts
@@ -3,7 +3,7 @@ import * as sinon from 'sinon';
 import chai from 'chai';
 import chaiHttp = require('chai-http');
 import { Request, Response } from 'express';
-import validateExpenditure from '../../../middlewares/validateExpenditure';
+import * as middlewares from '../../../middlewares/validateExpenditure';
 
 chai.use(chaiHttp);
 
@@ -13,120 +13,192 @@ const request = {} as Request;
 const response = {} as Response;
 let next = () => {};
 
-describe('Test expenditure middleware', () => {
-  describe('When expenditure is invalid', () => {
-    before(() => {
-      next = sinon.stub();
-      response.status = sinon.stub().returns(response);
-      response.json = sinon.stub();
+describe.only('Test expenditure middleware', () => {
+  describe('Test middleware to /post expenditure', () => {
+    describe('When expenditure is invalid', () => {
+      before(() => {
+        next = sinon.stub();
+        response.status = sinon.stub().returns(response);
+        response.json = sinon.stub();
+      });
+
+      it('When expenditure is not provided', () => {
+        request.body = { userId: '', date: '', category: '' };
+
+        middlewares.create(request, response, next);
+
+        expect((response.status as sinon.SinonStub).calledWith(400)).to.be.equal(true);
+        expect((response.json as sinon.SinonStub).calledWith({ message: '"expenditure" is required' })).to.be.equal(true);
+        expect((next as sinon.SinonStub).called).to.be.equal(false);
+      });
+
+      it('When expenditure is a string', () => {
+        request.body = { expenditure: 'string', userId: '', date: '', category: '' };
+
+        middlewares.create(request, response, next);
+
+        expect((response.status as sinon.SinonStub).calledWith(400)).to.be.equal(true);
+        expect((response.json as sinon.SinonStub).calledWith({ message: '"expenditure" must be a number' })).to.be.equal(true);
+        expect((next as sinon.SinonStub).called).to.be.equal(false);
+      });
+
+      it('When expenditure does not valid', () => {
+        request.body = { expenditure: 0, userId: '', date: '', category: '' };
+
+        middlewares.create(request, response, next);
+
+        expect((response.status as sinon.SinonStub).calledWith(400)).to.be.equal(true);
+        expect((response.json as sinon.SinonStub).calledWith({ message: '"expenditure" must be greater than or equal to 1' })).to.be.equal(true);
+        expect((next as sinon.SinonStub).called).to.be.equal(false);
+      });
     });
+    describe('When category is invalid', () => {
+      before(() => {
+        next = sinon.stub();
+        response.status = sinon.stub().returns(response);
+        response.json = sinon.stub();
+      });
 
-    it('When expenditure is not provided', () => {
-      request.body = { userId: '', date: '', category: '' };
+      it('When category is not provided', () => {
+        request.body = { expenditure: 1, userId: 1, date: '2022-12-01' };
 
-      validateExpenditure(request, response, next);
+        middlewares.create(request, response, next);
 
-      expect((response.status as sinon.SinonStub).calledWith(400)).to.be.equal(true);
-      expect((response.json as sinon.SinonStub).calledWith({ message: '"expenditure" is required' })).to.be.equal(true);
-      expect((next as sinon.SinonStub).called).to.be.equal(false);
+        expect((response.status as sinon.SinonStub).calledWith(400)).to.be.equal(true);
+        expect((response.json as sinon.SinonStub).calledWith({ message: '"category" is required' })).to.be.equal(true);
+        expect((next as sinon.SinonStub).called).to.be.equal(false);
+      });
+
+      it('When category is empty', () => {
+        request.body = { expenditure: 1, userId: 1, date: '2022-12-01', category: '' };
+
+        middlewares.create(request, response, next);
+
+        expect((response.status as sinon.SinonStub).calledWith(400)).to.be.equal(true);
+        expect((response.json as sinon.SinonStub).calledWith({ message: '"category" is not allowed to be empty' })).to.be.equal(true);
+        expect((next as sinon.SinonStub).called).to.be.equal(false);
+      });
+
+      it('When category is a number', () => {
+        request.body = { expenditure: 1, userId: 1, date: '2022-12-01', category: 0 };
+
+        middlewares.create(request, response, next);
+
+        expect((response.status as sinon.SinonStub).calledWith(400)).to.be.equal(true);
+        expect((response.json as sinon.SinonStub).calledWith({ message: '"category" must be a string' })).to.be.equal(true);
+        expect((next as sinon.SinonStub).called).to.be.equal(false);
+      });
     });
+    describe('When date is invalid', () => {
+      before(() => {
+        next = sinon.stub();
+        response.status = sinon.stub().returns(response);
+        response.json = sinon.stub();
+      });
 
-    it('When expenditure is a string', () => {
-      request.body = { expenditure: 'string', userId: '', date: '', category: '' };
+      it('When date is not provided', () => {
+        request.body = { expenditure: 1, category: '' };
 
-      validateExpenditure(request, response, next);
+        middlewares.create(request, response, next);
 
-      expect((response.status as sinon.SinonStub).calledWith(400)).to.be.equal(true);
-      expect((response.json as sinon.SinonStub).calledWith({ message: '"expenditure" must be a number' })).to.be.equal(true);
-      expect((next as sinon.SinonStub).called).to.be.equal(false);
+        expect((response.status as sinon.SinonStub).calledWith(400)).to.be.equal(true);
+        expect((response.json as sinon.SinonStub).calledWith({ message: '"date" is required' })).to.be.equal(true);
+        expect((next as sinon.SinonStub).called).to.be.equal(false);
+      });
+
+      it('When date does not valid', () => {
+        request.body = { expenditure: 1, date: 'DD-YYYY-MM', category: '' };
+
+        middlewares.create(request, response, next);
+
+        expect((response.status as sinon.SinonStub).calledWith(400)).to.be.equal(true);
+        expect((response.json as sinon.SinonStub).calledWith({ message: '"date" must be in YYYY-MM-DD format' })).to.be.equal(true);
+        expect((next as sinon.SinonStub).called).to.be.equal(false);
+      });
     });
+    describe('When expenditure is valid', () => {
+      before(() => {
+        next = sinon.stub();
+        response.status = sinon.stub().returns(response);
+        response.json = sinon.stub();
+      });
 
-    it('When expenditure does not valid', () => {
-      request.body = { expenditure: 0, userId: '', date: '', category: '' };
+      it('Next function should be called', () => {
+        request.body = { expenditure: 1, category: 'Pet', date: '2022-12-01' };
+        middlewares.create(request, response, next);
 
-      validateExpenditure(request, response, next);
-
-      expect((response.status as sinon.SinonStub).calledWith(400)).to.be.equal(true);
-      expect((response.json as sinon.SinonStub).calledWith({ message: '"expenditure" must be greater than or equal to 1' })).to.be.equal(true);
-      expect((next as sinon.SinonStub).called).to.be.equal(false);
+        expect((next as sinon.SinonStub).called).to.be.equal(true);
+      });
     });
   });
-  describe('When category is invalid', () => {
-    before(() => {
-      next = sinon.stub();
-      response.status = sinon.stub().returns(response);
-      response.json = sinon.stub();
+  describe('Test middleware to /get:id expediture', () => {
+    describe('When category is invalid', () => {
+      before(() => {
+        next = sinon.stub();
+        response.status = sinon.stub().returns(response);
+        response.json = sinon.stub();
+      });
+
+      it('When category is not provided', () => {
+        request.query = { date: '2022-12-01' };
+
+        middlewares.read(request, response, next);
+
+        expect((response.status as sinon.SinonStub).calledWith(400)).to.be.equal(true);
+        expect((response.json as sinon.SinonStub).calledWith({ message: '"category" is required' })).to.be.equal(true);
+        expect((next as sinon.SinonStub).called).to.be.equal(false);
+      });
+
+      it('When category is empty', () => {
+        request.query = { category: '', date: '2022-12-01' };
+
+        middlewares.read(request, response, next);
+
+        expect((response.status as sinon.SinonStub).calledWith(400)).to.be.equal(true);
+        expect((response.json as sinon.SinonStub).calledWith({ message: '"category" is not allowed to be empty' })).to.be.equal(true);
+        expect((next as sinon.SinonStub).called).to.be.equal(false);
+      });
     });
+    describe('When date is invalid', () => {
+      before(() => {
+        next = sinon.stub();
+        response.status = sinon.stub().returns(response);
+        response.json = sinon.stub();
+      });
 
-    it('When category is not provided', () => {
-      request.body = { expenditure: 1, userId: 1, date: '2022-12-01' };
+      it('When date is not provided', () => {
+        request.query = { category: 'Pet' };
 
-      validateExpenditure(request, response, next);
+        middlewares.read(request, response, next);
 
-      expect((response.status as sinon.SinonStub).calledWith(400)).to.be.equal(true);
-      expect((response.json as sinon.SinonStub).calledWith({ message: '"category" is required' })).to.be.equal(true);
-      expect((next as sinon.SinonStub).called).to.be.equal(false);
+        expect((response.status as sinon.SinonStub).calledWith(400)).to.be.equal(true);
+        expect((response.json as sinon.SinonStub).calledWith({ message: '"date" is required' })).to.be.equal(true);
+        expect((next as sinon.SinonStub).called).to.be.equal(false);
+      });
+
+      it('When date does not valid', () => {
+        request.query = { category: 'Pet', date: 'DD-YYYY-MM' };
+
+        middlewares.read(request, response, next);
+
+        expect((response.status as sinon.SinonStub).calledWith(400)).to.be.equal(true);
+        expect((response.json as sinon.SinonStub).calledWith({ message: '"date" must be in YYYY-MM-DD format' })).to.be.equal(true);
+        expect((next as sinon.SinonStub).called).to.be.equal(false);
+      });
     });
+    describe('When query is valid', () => {
+      before(() => {
+        next = sinon.stub();
+        response.status = sinon.stub().returns(response);
+        response.json = sinon.stub();
+      });
 
-    it('When category is empty', () => {
-      request.body = { expenditure: 1, userId: 1, date: '2022-12-01', category: '' };
+      it('Next function should be called', () => {
+        request.query = { category: 'Pet', date: '2022-12-01' };
+        middlewares.read(request, response, next);
 
-      validateExpenditure(request, response, next);
-
-      expect((response.status as sinon.SinonStub).calledWith(400)).to.be.equal(true);
-      expect((response.json as sinon.SinonStub).calledWith({ message: '"category" is not allowed to be empty' })).to.be.equal(true);
-      expect((next as sinon.SinonStub).called).to.be.equal(false);
-    });
-
-    it('When category is a number', () => {
-      request.body = { expenditure: 1, userId: 1, date: '2022-12-01', category: 0 };
-
-      validateExpenditure(request, response, next);
-
-      expect((response.status as sinon.SinonStub).calledWith(400)).to.be.equal(true);
-      expect((response.json as sinon.SinonStub).calledWith({ message: '"category" must be a string' })).to.be.equal(true);
-      expect((next as sinon.SinonStub).called).to.be.equal(false);
-    });
-  });
-  describe('When date is invalid', () => {
-    before(() => {
-      next = sinon.stub();
-      response.status = sinon.stub().returns(response);
-      response.json = sinon.stub();
-    });
-
-    it('When date is not provided', () => {
-      request.body = { expenditure: 1, category: '' };
-
-      validateExpenditure(request, response, next);
-
-      expect((response.status as sinon.SinonStub).calledWith(400)).to.be.equal(true);
-      expect((response.json as sinon.SinonStub).calledWith({ message: '"date" is required' })).to.be.equal(true);
-      expect((next as sinon.SinonStub).called).to.be.equal(false);
-    });
-
-    it('When date does not valid', () => {
-      request.body = { expenditure: 1, date: 'DD-YYYY-MM', category: '' };
-
-      validateExpenditure(request, response, next);
-
-      expect((response.status as sinon.SinonStub).calledWith(400)).to.be.equal(true);
-      expect((response.json as sinon.SinonStub).calledWith({ message: '"date" must be in YYYY-MM-DD format' })).to.be.equal(true);
-      expect((next as sinon.SinonStub).called).to.be.equal(false);
-    });
-  });
-  describe('When expenditure is valid', () => {
-    before(() => {
-      next = sinon.stub();
-      response.status = sinon.stub().returns(response);
-      response.json = sinon.stub();
-    });
-
-    it('Next function should be called', () => {
-      request.body = { expenditure: 1, category: 'Pet', date: '2022-12-01' };
-      validateExpenditure(request, response, next);
-
-      expect((next as sinon.SinonStub).called).to.be.equal(true);
+        expect((next as sinon.SinonStub).called).to.be.equal(true);
+      });
     });
   });
 });
