e1cd0d988dacfc2e0b2da06a72492e83ebc49eb8
Rodolpho Coller <rodolpho.coller12@gmail.com>
2022-06-16 22:41:59 -0300

    feat: create salary middlweate

---
+++ src/middlewares/validateSalary.ts
@@ -0,0 +1,12 @@
+import { Request, Response, NextFunction } from 'express';
+import * as schemas from '../schemas/salarySchema';
+
+export function create(req: Request, res: Response, next: NextFunction) {
+  const { value, date } = req.body;
+
+  const { error } = schemas.create.validate({ value, date });
+
+  if (error) return res.status(400).json({ message: error.message });
+
+  return next();
+}
---
+++ src/schemas/salarySchema.ts
@@ -0,0 +1,9 @@
+import JoiBase from 'joi';
+import JoiDate from '@joi/date';
+
+const Joi = JoiBase.extend(JoiDate);
+
+export const create = Joi.object({
+  value: Joi.number().min(1).required(),
+  date: Joi.date().format('YYYY-MM-DD').required(),
+});
