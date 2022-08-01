333f9bf945641231afa114102c985b112d298cc2
Rodolpho Coller <rodolpho.coller12@gmail.com>
2022-07-31 22:03:30 -0300

    clean comments

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -18,22 +18,3 @@ export async function readOneById(id: number) {
 
   return user;
 }
-
-// export async function updateCategory(id: number, categoryId: number) {
-//   const categories = await prisma.user.update({
-//     where: { id },
-//     data: {
-//       categories: { connect: { id: categoryId } },
-//     },
-//     select: { categories: true },
-//   });
-
-//   return categories;
-// }
-
-// export async function readCategory(id: number) {
-//   const categories = await prisma.user.findUnique(
-// { where: { id }, select: { categories: true } });
-
-//   return categories;
-// }
