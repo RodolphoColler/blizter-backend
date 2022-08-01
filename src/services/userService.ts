69d479123bae2cd52877faddb7db88f63554e654
Rodolpho Coller <rodolpho.coller12@gmail.com>
2022-07-31 22:04:37 -0300

    refact: now all routes use new error handler

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -2,11 +2,12 @@ import bcrypt from 'bcryptjs';
 import * as model from '../models/userModel';
 import { IUser } from '../interfaces/userInterface';
 import { jwtToken } from '../helpers/jwt';
+import { conflictError } from '../errors';
 
 export async function create({ email, password, name }: IUser) {
   const isUserExistent = await model.readOne(email);
 
-  if (isUserExistent) throw new Error('User already exist.');
+  if (isUserExistent) throw conflictError('User already exist.');
 
   const encryptedPassword = await bcrypt.hash(password, 10);
 
