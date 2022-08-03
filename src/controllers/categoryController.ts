ed7ab7724a523d0bff724e894ce092fa698efcb5
Rodolpho Coller <rodolpho.coller12@gmail.com>
2022-08-03 11:11:17 -0300

    refactor categoryController error treatment

--- src/controllers/categoryController.ts
+++ src/controllers/categoryController.ts
@@ -1,12 +1,12 @@
-import { Response, Request } from 'express';
+import { Response, Request, NextFunction } from 'express';
 import * as service from '../services/categoryService';
 
-export async function read(_req: Request, res: Response) {
+export async function read(_req: Request, res: Response, next: NextFunction) {
   try {
     const categories = await service.read();
 
     return res.status(200).json({ categories });
   } catch (error) {
-    return res.status(500).json({ message: 'Inside server error.' });
+    return next(error);
   }
 }
