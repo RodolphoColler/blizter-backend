94c0b456c1fcece157a3214ea778708cdfed12fe
Rodolpho Coller <rodolpho.coller12@gmail.com>
2022-08-03 11:12:02 -0300

    refactor salaryController error treatment and aply rest

--- src/controllers/salaryController.ts
+++ src/controllers/salaryController.ts
@@ -1,13 +1,8 @@
-import { Response, Request } from 'express';
+import { Response, Request, NextFunction } from 'express';
 import { IQuerySalary } from '../interfaces/salaryInterface';
 import * as service from '../services/salaryService';
 
-enum errors {
-  'User not exists.' = 400,
-  'Salary not exists.' = 400,
-}
-
-export async function create(req: Request, res: Response) {
+export async function create(req: Request, res: Response, next: NextFunction) {
   try {
     const { value, date } = req.body;
     const { id: userId } = req.tokenPayload;
@@ -15,33 +10,25 @@ export async function create(req: Request, res: Response) {
     const createdSalary = await service.create({ value, userId, date });
 
     return res.status(201).json({ salary: createdSalary });
-  } catch (error: any) {
-    const { message } = error;    
-
-    if (message in errors) return res.status(Number(errors[message])).json({ message });
-
-    return res.status(500).json({ message: 'Inside server error.' });
+  } catch (error) {
+    return next(error);
   }
 }
 
-export async function readOne(req: Request, res: Response) {
+export async function read(req: Request, res: Response, next: NextFunction) {
   try {
     const { date } = req.query as unknown as IQuerySalary;
-    const { id } = req.params;
+    const { id: userId } = req.tokenPayload;
 
-    const salary = await service.readOne({ userId: Number(id), date });
+    const salary = await service.read({ userId, date });
 
     return res.status(200).json({ salary });
-  } catch (error: any) {
-    const { message } = error;
-
-    if (message in errors) return res.status(Number(errors[message])).json({ message });
-
-    return res.status(500).json({ message: 'Inside server error.' });
+  } catch (error) {
+    return next(error);
   }
 }
 
-export async function updateOne(req: Request, res: Response) {
+export async function updateOne(req: Request, res: Response, next: NextFunction) {
   try {
     const { value } = req.body;
     const { id } = req.params;
@@ -49,11 +36,7 @@ export async function updateOne(req: Request, res: Response) {
     const salary = await service.updateOne({ id: Number(id), value });
 
     return res.status(200).json({ salary });
-  } catch (error: any) {
-    const { message } = error;
-
-    if (message in errors) return res.status(Number(errors[message])).json({ message });
-
-    return res.status(500).json({ message: 'Inside server error.' });
+  } catch (error) {
+    return next(error);
   }
 }
