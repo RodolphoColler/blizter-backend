ed45730f539887c3a38ac27cd20d9ab42fa82130
Rodolpho Coller <rodolpho.coller12@gmail.com>
2022-08-12 09:49:14 -0300

    feat: config session

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
 import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import session from 'express-session';
+import dotenv from 'dotenv';
 import * as router from './routes';
 import errorHandle from './middlewares/errorMiddleware';
+
+dotenv.config();
 
 const app = express();
 
@@ -10,10 +14,19 @@ app.use(express.json());
 
 app.use(cookieParser());
 
+const sessionConfig = {
+  secret: process.env.JWT_SECRET || '',
+  cookie: {
+    sameSite: false,
+  },
+};
+
 const corsConfig = {
   origin: ['http://localhost:3000', 'https://blizter.vercel.app'],
   credentials: true,
 };
+
+app.use(session(sessionConfig));
 
 app.use(cors(corsConfig));
 
