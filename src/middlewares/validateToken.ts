03620af6e4d64bdbaa73875b1ff387a0df6408be
Rodolpho Coller <rodolpho.coller12@gmail.com>
2022-08-11 22:11:55 -0300

    refact: token middlweware now receive token from cookies

--- src/middlewares/validateToken.ts
+++ src/middlewares/validateToken.ts
@@ -2,12 +2,12 @@ import { NextFunction, Response, Request } from 'express';
 import { jwtVerify } from '../helpers/jwt';
 
 export default async (req: Request, res: Response, next: NextFunction) => {
-  const { authorization } = req.headers;
-
-  if (!authorization) return res.status(400).json({ message: 'Your request must have a token.' });
-
   try {
-    req.tokenPayload = jwtVerify(authorization);
+    const { token } = req.cookies;
+
+    if (!token) return res.status(400).json({ message: 'Your request must have a token.' });
+
+    req.tokenPayload = jwtVerify(token);
 
     return next();
   } catch (error) {
