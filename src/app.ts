8117f335f37ae2c99a61e1b1b005b3365d46e3de
Rodolpho Coller <rodolpho.coller12@gmail.com>
2022-08-19 17:28:34 -0300

    starting to rename login route to signIn

--- src/app.ts
+++ src/app.ts
@@ -23,7 +23,7 @@ app.use(cors(corsConfig));
 app.use('/expenditure', router.expenditure);
 app.use('/category', router.category);
 app.use('/salary', router.salary);
-app.use('/login', router.login);
+app.use('/signin', router.signIn);
 app.use('/user', router.user);
 app.use('/token', router.token);
 
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,5 @@
 export { default as user } from './user';
-export { default as login } from './login';
+export { default as signIn } from './signIn';
 export { default as category } from './category';
 export { default as expenditure } from './expenditure';
 export { default as salary } from './salary';
