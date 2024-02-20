1e30f7c39500431b60596554422d65b08768eee4
Rodolpho Coller <rodolpho.coller12@gmail.com>
2024-02-20 15:49:03 -0300

    fix: cors origin

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 
 const corsConfig = {
-  origin: ['http://localhost:3000', 'https://blizter.vercel.app'],
+  origin: 'https://blizter.vercel.app',
   credentials: true,
 };
 
