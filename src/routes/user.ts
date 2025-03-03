94da18adcd98fda64f6beb675f6d22ff6c9dd04c
RodolphoColler <rodolpho.coller12@gmail.com>
2025-03-03 15:14:32 -0300

    reverting changes

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,10 +1,14 @@
 import { Router, Request, Response } from 'express';
-import prisma from '../models/prisma';
+// import prisma from '../models/prisma';
 
 const router = Router();
 
 router.get('/', async (_req: Request, res: Response) => {
-  const categories = await prisma.category.findMany();
+  // const categories = await prisma.category.findMany();
+
+  const categories = {
+    a: 'qq',
+  };
 
   return res.status(201).json({ categories });
 });
